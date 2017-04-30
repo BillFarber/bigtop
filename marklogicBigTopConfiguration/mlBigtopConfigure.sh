399c84eb051629eebebf5ed4cc10efb283505e36
Phil Barber <Phil.Barber@marklogic.com>
2017-04-30 00:21:45 -0400

    Clean up the ML BigTop config scripts and add the REST call to create/destroy the Certificate Template.

--- marklogicBigTopConfiguration/mlBigtopConfigure.sh
+++ marklogicBigTopConfiguration/mlBigtopConfigure.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 ################################################################
-# This script initializes MarkLogic and creates the admin account.
+# This script configures MarkLogic for the BigTop LDAP demo
 #
 # Usage:
 #
-# initialize-ml.sh -u <admin-username> -p <admin-password> -r <secuity-realm>
+# initialize-ml.sh -u <admin-username> -p <admin-password> -h <host-name>
 #     -u  The admin username to create
 #     -p  The password for the created admin username
-#     -r  The desired security realm string, if any. Default: "public"
-################################################################
-
-################################################################
-# The following are used to create the first MarkLogic
-# Administrator account. The values are passed into this script
-# from commandline arguements.
+#     -h  The hostname of the MarkLogic server
 ################################################################
 USER=""
 PASS=""
-SEC_REALM="public"
+ML_HOST=""
 
 ################################################################
 # Default curl authorization mode. MarkLogic uses "digest" as
@@ -31,23 +25,16 @@ N_RETRY=5
 # has restarted.
 RETRY_INTERVAL=10
 
-# Get the hostname of this server.
-ML_HOST=$HOSTNAME
-
 #######################################################
-# Parse the command line. After MarkLogic initializes,
-# an administrator account must be created. The desired
-# username, password and realm for the admin account is
-# given in commandline arguements.
+# Parse the command line.
 ########################################################
-
-# Check that at least 2 arguments were given for the
-# admin and password. If not, exit with error.
-if [ $# -ge 2 ]; then 
+# Check that at least 5 arguments were given for the
+# admin, password, and host. If not, exit with error.
+if [ $# -ge 5 ]; then 
   OPTIND=1
-  while getopts ":r:p:u:" opt; do
+  while getopts ":h:p:u:" opt; do
     case "$opt" in
-      r) SEC_REALM=$OPTARG ;;
+      h) ML_HOST=$OPTARG ;;
       p) PASS=$OPTARG ;;
       u) USER=$OPTARG ;;
       \?) echo "Unrecognized option: -$OPTARG" >&2; exit 1 ;;
@@ -55,11 +42,14 @@ if [ $# -ge 2 ]; then
   done
   shift $((OPTIND-1))
 else
-  echo "ERROR: Desired admin username and password are required." >&2
-  echo "USAGE: initialize-ml.sh -u <admin-username> -p <admin-password> [-r <security-realm-string>]" >&2
+  echo "ERROR: Desired admin username and password and host are required." >&2
+  echo "USAGE: initialize-ml.sh -u <admin-username> -p <admin-password> -h <host-name>" >&2
   exit 1
 fi
 
+#######################################################
+# Build base CURL commands
+########################################################
 # Curl command for all requests. Suppress progress meter (-s),
 # but still show errors (-S)
 CURL="curl -s -S"
@@ -103,24 +93,29 @@ echo "Create default REST server"
 REST_CREATE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"rest-api": { "name": "BigTopServer", "port": "8011", "database": "BigTopContent", "modules-database": "BigTopModules" } }' \
 	http://$ML_HOST:8002/v1/rest-apis`
-echo "Create REST server response: $REST_CREATE_REPLY"
+#echo "Create default REST server response: $REST_CREATE_REPLY"
 
 echo "Request restart"
 REST_RESTART_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"operation": "restart-local-cluster"}' http://$ML_HOST:8002/manage/v2`
-echo "Restart response: $REST_RESTART_REPLY"
+#echo "Restart response: $REST_RESTART_REPLY"
 
 TIMESTAMP=`$AUTH_CURL "http://$ML_HOST:8001/admin/v1/timestamp"`
 echo "After ML start TIMESTAMP=$TIMESTAMP"
 
 echo "Create LDAP external security"
 CREATE_LDAP_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
-	-d @/vagrant/resources/marklogic/ExternalConfig.json http://$ML_HOST:8002/manage/v2/external-security`
-echo "Create LDAP external security response: $CREATE_LDAP_REPLY"
+	-d @ExternalConfig.json http://$ML_HOST:8002/manage/v2/external-security`
+#echo "Create LDAP external security response: $CREATE_LDAP_REPLY"
+
+echo "Create BigTopCertTemplate"
+CREATE_TEMPLATE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
+  -d @BigTopCertTemplate.json http://$ML_HOST:8002/manage/v2/certificate-templates`
+#echo "Create BigTopCertTemplate response: $CREATE_TEMPLATE_REPLY"
 
 echo "Update the REST server with the new LDAP external security"
 UPDATE_SERVER_REPLY=`$AUTH_CURL -X PUT -H "Content-type: application/json" \
-	-d '{"authentication":"basic", "internal-security":false,"default-user":"nobody","external-security":"BigTop-LDAP-security" }' \
+	-d '{"authentication":"basic", "internal-security":false,"default-user":"nobody","external-security":"BigTop-LDAP-security", "ssl-require-client-certificate": false }' \
 	http://$ML_HOST:8002/manage/v2/servers/BigTopServer/properties?group-id=Default`
 echo "Update the REST server with the new LDAP external security response: $UPDATE_SERVER_REPLY"
 
@@ -130,7 +125,7 @@ echo "After ML start TIMESTAMP=$TIMESTAMP"
 echo "Request restart"
 REST_RESTART_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"operation": "restart-local-cluster"}' http://$ML_HOST:8002/manage/v2`
-echo "Restart response: $REST_RESTART_REPLY"
+#echo "Restart response: $REST_RESTART_REPLY"
 
 restart_check $ML_HOST $TIMESTAMP 95
 
@@ -138,24 +133,24 @@ echo "Create BigTopAdminRole to give users the rest-reader execute privilege and
 CREATE_ROLE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"role-name":"BigTopAdminRole", "external-name":["cn=bigtopadmingroup,cn=groups,cn=accounts,dc=bigtop,dc=local"], "role":["rest-writer"]}' \
 	http://$ML_HOST:8002/manage/v2/roles`
-echo "Create BigTopAdminRole response: $CREATE_ROLE_REPLY"
+#echo "Create BigTopAdminRole response: $CREATE_ROLE_REPLY"
 
 echo "Create BigTopUsersRole to give users the rest-reader execute privilege and access"
 CREATE_ROLE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"role-name":"BigTopUsersRole", "external-name":["cn=bigtopusers,cn=groups,cn=accounts,dc=bigtop,dc=local"], "privilege":[{"privilege-name":"rest-reader", "action":"http://marklogic.com/xdmp/privileges/rest-reader","kind":"execute"}]}' \
 	http://$ML_HOST:8002/manage/v2/roles`
-echo "Create BigTopUsersRole response: $CREATE_ROLE_REPLY"
+#echo "Create BigTopUsersRole response: $CREATE_ROLE_REPLY"
 
 echo "Create BigTopReaderRole_red to give users the rest-reader execute privilege and access"
 CREATE_ROLE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"role-name":"BigTopReaderRole_red", "external-name":["cn=bigtopreaderredgroup,cn=groups,cn=accounts,dc=bigtop,dc=local"]}' \
 	http://$ML_HOST:8002/manage/v2/roles`
-echo "Create BigTopReaderRole_red response: $CREATE_ROLE_REPLY"
+#echo "Create BigTopReaderRole_red response: $CREATE_ROLE_REPLY"
 
 echo "Create BigTopReaderRole_blue to give users the rest-reader execute privilege and access"
 CREATE_ROLE_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"role-name":"BigTopReaderRole_blue", "external-name":["cn=bigtopreaderbluegroup,cn=groups,cn=accounts,dc=bigtop,dc=local"]}' \
 	http://$ML_HOST:8002/manage/v2/roles`
-echo "Create BigTopReaderRole_blue response: $CREATE_ROLE_REPLY"
+#echo "Create BigTopReaderRole_blue response: $CREATE_ROLE_REPLY"
 
 restart_check $ML_HOST $TIMESTAMP 95
--- marklogicBigTopConfiguration/mlBigtopWipe.sh
+++ marklogicBigTopConfiguration/mlBigtopWipe.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 ################################################################
-# This script initializes MarkLogic and creates the admin account.
+# This script removes the BigTop LDAP demo from MarkLogic.
 #
 # Usage:
 #
-# initialize-ml.sh -u <admin-username> -p <admin-password> -r <secuity-realm>
+# initialize-ml.sh -u <admin-username> -p <admin-password> -h <host-name>
 #     -u  The admin username to create
 #     -p  The password for the created admin username
-#     -r  The desired security realm string, if any. Default: "public"
-################################################################
-
-################################################################
-# The following are used to create the first MarkLogic
-# Administrator account. The values are passed into this script
-# from commandline arguements.
+#     -h  The hostname of the MarkLogic server
 ################################################################
 USER=""
 PASS=""
-SEC_REALM="public"
+ML_HOST=""
 
 ################################################################
 # Default curl authorization mode. MarkLogic uses "digest" as
@@ -31,23 +25,16 @@ N_RETRY=5
 # has restarted.
 RETRY_INTERVAL=10
 
-# Get the hostname of this server.
-ML_HOST=$HOSTNAME
-
 #######################################################
-# Parse the command line. After MarkLogic initializes,
-# an administrator account must be created. The desired
-# username, password and realm for the admin account is
-# given in commandline arguements.
+# Parse the command line.
 ########################################################
-
-# Check that at least 2 arguments were given for the
-# admin and password. If not, exit with error.
-if [ $# -ge 2 ]; then 
+# Check that at least 5 arguments were given for the
+# admin, password, and host. If not, exit with error.
+if [ $# -ge 5 ]; then 
   OPTIND=1
-  while getopts ":r:p:u:" opt; do
+  while getopts ":h:p:u:" opt; do
     case "$opt" in
-      r) SEC_REALM=$OPTARG ;;
+      h) ML_HOST=$OPTARG ;;
       p) PASS=$OPTARG ;;
       u) USER=$OPTARG ;;
       \?) echo "Unrecognized option: -$OPTARG" >&2; exit 1 ;;
@@ -55,11 +42,14 @@ if [ $# -ge 2 ]; then
   done
   shift $((OPTIND-1))
 else
-  echo "ERROR: Desired admin username and password are required." >&2
-  echo "USAGE: initialize-ml.sh -u <admin-username> -p <admin-password> [-r <security-realm-string>]" >&2
+  echo "ERROR: Desired admin username and password, and host are required." >&2
+  echo "USAGE: initialize-ml.sh -u <admin-username> -p <admin-password> -h <host-name>" >&2
   exit 1
 fi
 
+#######################################################
+# Build base CURL commands
+########################################################
 # Curl command for all requests. Suppress progress meter (-s),
 # but still show errors (-S)
 CURL="curl -s -S"
@@ -102,46 +92,51 @@ echo "After ML start TIMESTAMP=$TIMESTAMP"
 echo "Delete BigTopServer"
 REST_DELETE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/servers/BigTopServer?group-id=Default`
-echo "Delete BigTopServer response: $REST_DELETE_REPLY"
+#echo "Delete BigTopServer response: $REST_DELETE_REPLY"
 
 echo "Request restart"
 REST_RESTART_REPLY=`$AUTH_CURL -X POST -H "Content-Type:application/json" \
 	-d '{"operation": "restart-local-cluster"}' http://$ML_HOST:8002/manage/v2`
-echo "Restart response: $REST_RESTART_REPLY"
+#echo "Restart response: $REST_RESTART_REPLY"
 
 restart_check $ML_HOST $TIMESTAMP 95
 
 echo "Delete BigTopContent"
 DELETE_CONTENT_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/databases/BigTopContent?forest-delete=data`
-echo "Delete BigTopServer response: $DELETE_CONTENT_REPLY"
+#echo "Delete BigTopContent response: $DELETE_CONTENT_REPLY"
 
 echo "Delete BigTopModules"
 DELETE_MODULES_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/databases/BigTopModules?forest-delete=data`
-echo "Delete BigTopModules response: $DELETE_MODULES_REPLY"
+#echo "Delete BigTopModules response: $DELETE_MODULES_REPLY"
 
 echo "Delete LDAP external security"
 DELETE_LDAP_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/external-security/BigTop-LDAP-security`
-echo "Delete LDAP external security response: $DELETE_LDAP_REPLY"
+#echo "Delete LDAP external security response: $DELETE_LDAP_REPLY"
 
 echo "Delete BigTopAdminRole role"
 DELETE_ROLE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/roles/BigTopAdminRole`
-echo "Delete BigTopAdminRole role response: $DELETE_ROLE_REPLY"
+#echo "Delete BigTopAdminRole role response: $DELETE_ROLE_REPLY"
 
 echo "Delete BigTopUsersRole role"
 DELETE_ROLE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/roles/BigTopUsersRole`
-echo "Delete BigTopUsersRole role response: $DELETE_ROLE_REPLY"
+#echo "Delete BigTopUsersRole role response: $DELETE_ROLE_REPLY"
 
 echo "Delete BigTopReaderRole_red role"
 DELETE_ROLE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/roles/BigTopReaderRole_red`
-echo "Delete BigTopReaderRole_red role response: $DELETE_ROLE_REPLY"
+#echo "Delete BigTopReaderRole_red role response: $DELETE_ROLE_REPLY"
 
 echo "Delete BigTopReaderRole_blue role"
 DELETE_ROLE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
 	http://$ML_HOST:8002/manage/v2/roles/BigTopReaderRole_blue`
-echo "Delete BigTopReaderRole_blue role response: $DELETE_ROLE_REPLY"
+#echo "Delete BigTopReaderRole_blue role response: $DELETE_ROLE_REPLY"
+
+echo "Delete the SSL Certificate Template"
+DELETE_CERTIFICATE_REPLY=`$AUTH_CURL -X DELETE -H "Content-Type:application/json" \
+	http://$ML_HOST:8002/manage/v2/certificate-templates/BigTopCertTemplate`
+#echo "Delete the SSL Certificate Template response: $DELETE_CERTIFICATE_REPLY"
